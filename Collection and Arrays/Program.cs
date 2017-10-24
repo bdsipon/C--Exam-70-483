63d537862f34be5974d47e46e88e8c1547b14cb1
bdsipon <sipon.here@gmail.com>
2017-10-24 13:28:29 +1000

    updated the collection

---
+++ Collection and Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_and_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a collection that is a list of strings
+
+            var dogs = new List<string>();
+            dogs.Add("Bulldog");
+            dogs.Add("Collie");
+            dogs.Add("Retriever");
+
+            // foreach to move through the list
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + "");
+            }
+        }
+    }
+}
