dd622a871784644f460dd639deea2b28a3c0fec7
bdsipon <sipon.here@gmail.com>
2017-10-25 03:12:41 +1000

    Created Static

---
+++ StaticExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Math.Round(35.3);
+            Console.WriteLine(r);
+            Car.Accelerate();
+          
+        }
+    }
+    public class Car
+    {
+        public static void Accelerate()
+        {
+            Console.WriteLine("Car is accelerating");
+        }
+
+    }
+}
