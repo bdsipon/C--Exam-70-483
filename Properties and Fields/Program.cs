7ff075234c9068287f2ea48a58a2a23b49a24d14
bdsipon <sipon.here@gmail.com>
2017-10-25 02:15:41 +1000

    Properties and fields

---
+++ Properties and Fields/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_and_Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Age = 20;
+            Console.WriteLine("Age:" + p.Age);
+            
+    }
+    }
+    public class Person
+    {
+    
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set {
+                if((value>0) && (value<65))
+                {
+                    age = value;
+                }
+
+                else
+                {
+                    throw new Exception("age can not be over 65");
+                }
+
+            }
+        }
+
+    }
+}
