0de7b329f6ad155e289f09042d52183c55b115d3
bdsipon <sipon.here@gmail.com>
2017-10-25 03:02:19 +1000

    Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DefConstructor dc = new DefConstructor();
+            //dc.DoThis();
+
+            AddConstruction ac = new AddConstruction();
+        }
+    }
+
+    public class DefConstructor
+    {
+        public void DoThis()
+        {
+            Console.WriteLine("DoThis has run..");
+        }
+    }
+
+    public class AddConstruction
+    {
+        public AddConstruction()
+        {
+            Console.WriteLine("The constructor has been called ...");
+            DoThat();
+        }
+        public void DoThat()
+        {
+            Console.WriteLine("DoThat has executed");
+
+        }
+    }
+}
