e2e45221004827a14009185035e4d7f1a1cb9372
bdsipon <sipon.here@gmail.com>
2017-10-25 12:10:56 +1000

    Created Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            list1.Add(3);
+            list1.Add(5);
+            list1.Add(12);
+            //list1.Add("Hello");
+            Console.WriteLine("Non-generic List Results....");
+            foreach(int x in list1)
+            {
+                Console.WriteLine(x);
+            }
+
+           
+          
+
+        }
+    }
+}
