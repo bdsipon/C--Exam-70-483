d95c76758417e7f8c215e9a7d3ab5876a3c4e36a
bdsipon <sipon.here@gmail.com>
2017-10-25 11:29:47 +1000

    Created Struct

---
+++ StructExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sipon s = new Sipon(6, 7);
+            s.DoThis();
+        }
+    }
+
+    public struct Sipon
+    {
+        public int x;
+        public int y;
+
+        public Sipon(int a, int b)
+        {
+            x = a;
+            y = b;
+
+        }
+
+        public void DoThis()
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
