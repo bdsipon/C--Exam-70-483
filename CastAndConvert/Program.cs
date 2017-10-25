e8d8b6959d91cbce496a53c471d4f83c664a757f
bdsipon <sipon.here@gmail.com>
2017-10-26 00:13:27 +1000

    Added Cast and Convert

---
+++ CastAndConvert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastAndConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            double y = 12.6;
+            x = Convert.ToInt32(y);
+            Console.WriteLine(x);
+
+            string str = "123";
+            int r;
+            r = Convert.ToInt32(str);
+            Console.WriteLine(r);
+        }
+    }
+}
